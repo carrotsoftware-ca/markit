be350f8fa18432a4556dc2663cc17dce5e5c3bf4
Tim O'Callaghan <tocallaghan@gmail.com>
2026-02-23 15:58:38 -0500

    feat: add project ready for actions

--- src/app/(contractor)/projects/add-project.tsx
+++ src/app/(contractor)/projects/add-project.tsx
@@ -1,28 +1,45 @@
+import NewProjectForm from "@/src/components/forms/NewProjectForm";
 import AuthScreenWrapper from "@/src/components/ui/AuthScreenWrapper";
 import FormWrapper from "@/src/components/ui/FormWrapper";
+import { useProjects } from "@/src/context/ProjectsContext";
+import { useTheme } from "@/src/context/ThemeContext";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AddProject() {
-  const handleSubmit = (data) => {
-    // TODO: handle project creation logic
-    console.log("New project submitted:", data);
+  const { createProject, loading } = useProjects();
+  const { theme } = useTheme();
+
+  const handleCreateProject = async () => {
+    await createProject();
   };
 
   return (
     <AuthScreenWrapper title="New Project" subtitle="Create a new project">
-      <FormWrapper>
-        
-      </FormWrapper>
+      <SafeAreaView style={{ flex: 1, padding: 10 }}>
+        <View style={{ flex: 1 }}>
+          <MaterialCommunityIcons
+            name="hammer-screwdriver"
+            size={300}
+            color={theme.colors.safetyOrange}
+            style={styles.watermark}
+          />
+          <FormWrapper style={{ flex: 1 }}>
+            <NewProjectForm onSubmit={handleCreateProject} />
+          </FormWrapper>
+        </View>
+      </SafeAreaView>
     </AuthScreenWrapper>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 24,
+  watermark: {
+    position: "absolute",
+    bottom: 30,
+    right: -20,
+    opacity: 0.07,
   },
 });
