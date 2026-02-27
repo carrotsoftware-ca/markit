747a5eacf26554d4311b5e38e5c2c0bb6848cb23
Tim O'Callaghan <tocallaghan@gmail.com>
2026-02-26 19:56:03 -0500

    dummy playstore account

--- src/services/auth/email.tsx
+++ src/services/auth/email.tsx
@@ -1,3 +1,9 @@
+/*
+dummy google account
+playstore@markitquote.com
+XHKyd0xezMuCpVBpEv8R
+*/
+
 export async function login({ email, password }) {
   // TODO: Implement email/password sign-in logic
   return { id: "email-123", displayName: "Email User", email };
